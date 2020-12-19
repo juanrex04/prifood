d3118667f3107965895b364ba76d5ce33be4ae27
Juan Manuel <juanpix2404@gmail.com>
2020-12-19 17:02:38 -0500

    Creacion proyecto y NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar id="navBar" toggleable="lg" type="dark">
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand :to="{ name: 'Home' }">PRIFOOD</b-navbar-brand>
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item :to="{ name: 'About' }">Acerca de PriFood</b-nav-item>
+            <b-nav-item :to="{ name: '' }">Productos</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <b-container>
+      <transition
+        mode="out-in"
+        enter-active-class="animate__animated animate__backInDown"
+      >
+        <router-view />
+      </transition>
+    </b-container>
   </div>
 </template>
 
+<script>
+export default {
+data() {
+  return {
+    color: e9ce51
+  }
+},
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  min-height: 100vh;
+  max-height: 200vh;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#navBar {
+  background-color: #13132f;
+  font-weight: 800;
+}
+#test{
+color: #e9ce51;
 }
 </style>
