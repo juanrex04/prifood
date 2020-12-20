e4a0a4c112bd7584fab78140f89462d14871ee73
Juan Manuel <juanpix2404@gmail.com>
2020-12-19 22:28:32 -0500

    -Se agregaron imaganes del menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,103 @@
 <template>
-  <div class="container my-3">
-
+  <div class="my-3">
+    <b-carousel
+      v-model="slide"
+      indicators
+      style="text-shadow: 0px 0px 2px #000"
+      fade
+      :interval="4000"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide>
+        <template #img>
+          <b-img
+            id="test"
+            class="d-block img-fluid"
+            fluid
+            :src="require('@/assets/img/menu1.jpg')"
+            alt="Fluid"
+            center
+            >Hola</b-img
+          >
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <b-img
+            id="test"
+            class="d-block img-fluid"
+            fluid
+            :src="require('@/assets/img/menu2.jpg')"
+            alt="Fluid"
+            center
+          />
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <b-img
+            id="test"
+            class="d-block img-fluid"
+            fluid
+            :src="require('@/assets/img/menu3.jpg')"
+            alt="Fluid"
+            center
+          />
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <b-img
+            id="test"
+            class="d-block img-fluid"
+            fluid
+            :src="require('@/assets/img/menu4.jpg')"
+            alt="Fluid"
+            center
+          />
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <b-img
+            id="test"
+            class="d-block img-fluid"
+            fluid
+            :src="require('@/assets/img/menu5.jpg')"
+            alt="Fluid"
+            center
+          />
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {},
+  data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
 };
 </script>
 
 <style lang="scss">
-
+#test {
+  width: auto;
+  min-height: 200px;
+  max-height: 480px;
+}
 </style>
